2445710e21f2dba3c4e576b8fae4d67910b670d7
Chintan Karki <karkichintan2000@gmail.com>
2021-07-21 07:10:51 +0545

    Added a fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,28 @@
+// **
+// *    The most efficient fizzbuzz algorithm 🙂🙂
+// *
+const fizzBuzzFunction = (inputNumber) => {
+  let multipleof3 = 3;
+  let multipleof5 = 5;
+  let iterator = 1;
+  while (iterator <= inputNumber) {
+    output = "";
+    if (iterator === multipleof3) {
+      output += "Fizz";
+      multipleof3 += 3;
+    }
+    if (iterator === multipleof5) {
+      output += "Buzz";
+      multipleof5 += 5;
+    }
+    output += String(iterator);
+    console.log(output);
+    iterator++;
+  }
+};
+
+console.time("fizzBuzzFunction");
+
+fizzBuzzFunction(30000); // <---- The function you're measuring time for
+
+console.timeEnd("fizzBuzzFunction");
