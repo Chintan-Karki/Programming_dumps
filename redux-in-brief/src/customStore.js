a6b2381f4cec0fe9920e52338a75e62b558a87ea
Chintan Karki <karkichintan2000@gmail.com>
2021-08-10 08:29:26 +0545

    Creating the subscriptions

--- redux-in-brief/src/customStore.js
+++ redux-in-brief/src/customStore.js
@@ -1,11 +1,28 @@
-function createStore() {
+import reducer from "./reducer";
+function createStore(reducer) {
 	let state;
+	let listeners = [];
+
 	function getState() {
 		return state;
 	}
+
+	function subscribe(listener) {
+		listeners.push(listener);
+	}
+
+	function dispatch(action) {
+		state = reducer(state, action);
+		for (let i = 0; i < listeners.length; i++) {
+			listeners[i]();
+		}
+	}
+
 	return {
+		subscribe,
+		dispatch,
 		getState,
 	};
 }
 
-export default createStore();
+export default createStore(reducer);
--- redux-in-brief/src/index.js
+++ redux-in-brief/src/index.js
@@ -1,4 +1,11 @@
 import store from "./customStore";
+import * as actions from "./actions";
 
-store.state = 1;
+store.subscribe(() => {
+	console.log("Store Changed!");
+});
+
+store.dispatch(actions.bugAdded("bug 1"));
+
 console.log(store);
+console.log(store.getState());
