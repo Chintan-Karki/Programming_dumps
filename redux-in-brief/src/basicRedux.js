eb2356197efc0f04aad2e17d896857c153d16a00
Chintan Karki <karkichintan2000@gmail.com>
2021-08-10 08:09:12 +0545

    Creating own custom store

---
+++ redux-in-brief/src/basicRedux.js
@@ -0,0 +1,23 @@
+import { bugAdded, bugRemoved, bugResolved } from "./actions";
+import store from "./store";
+
+console.log("Starter state of the store: ", store.getState());
+
+const unsubscribe = store.subscribe(() => {
+	console.log("Store Changed", store.getState());
+});
+
+console.log("Bug Added through action dispatch");
+store.dispatch(bugAdded("First bug"));
+console.log("Another Bug Added through action dispatch");
+store.dispatch(bugAdded("Second bug"));
+console.log("Bug Resolved through action dispatch");
+store.dispatch(bugResolved(2));
+console.log("Bug Removed through action dispatch");
+store.dispatch(bugRemoved(1));
+console.log("Another Bug Removed through another action dispatch");
+store.dispatch(bugRemoved(2));
+console.log("Final State");
+console.log(store.getState());
+
+// unsubscribe();
