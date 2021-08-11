3a3a814e153fbcc5ffc96874a1095ebbc387cb26
Chintan Karki <karkichintan2000@gmail.com>
2021-08-11 14:53:48 +0545

    Redux toolkit default middleware

--- redux-in-brief/redux-concepts/src/store/configureStore.js
+++ redux-in-brief/redux-concepts/src/store/configureStore.js
@@ -1,13 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 // import { createStore } from "redux";
 import reducer from "./reducer";
 import logger from "./middleware/logger";
-import func from "./middleware/func";
 
 export default function () {
 	return configureStore({
 		reducer,
-		middleware: [logger({ justADemoParameter: 20 }), func],
+		middleware: [...getDefaultMiddleware(), logger({ justADemoParameter: 20 })],
 		//we can also use redux's 'applyMiddleware(logger)' in place of redux-toolkit's'middleware: [logger]'
 	});
 }
