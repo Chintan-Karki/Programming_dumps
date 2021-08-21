4cfcb7a1718b9d9a3abd786fbc3075727c73ded9
Chintan Karki <karkichintan2000@gmail.com>
2021-08-21 11:55:49 +0545

    refactoring

--- chessBoard/chessBoard.java
+++ chessBoard/chessBoard.java
@@ -24,27 +24,27 @@ public chessBoard() {
         }
     }
 
-    squares[0][0].add(new JLabel(new ImageIcon("rookblack.png")));
-    squares[0][1].add(new JLabel(new ImageIcon("knightblack.png")));
-    squares[0][2].add(new JLabel(new ImageIcon("bishopblack.png")));
-    squares[0][3].add(new JLabel(new ImageIcon("queenblack.png")));
-    squares[0][4].add(new JLabel(new ImageIcon("kingblack.png")));
-    squares[0][5].add(new JLabel(new ImageIcon("bishopblack.png")));
-    squares[0][6].add(new JLabel(new ImageIcon("knightblack.png")));
-    squares[0][7].add(new JLabel(new ImageIcon("rookblack.png")));
+    squares[0][0].add(new JLabel(new ImageIcon("./assets/rookblack.png")));
+    squares[0][1].add(new JLabel(new ImageIcon("./assets/knightblack.png")));
+    squares[0][2].add(new JLabel(new ImageIcon("./assets/bishopblack.png")));
+    squares[0][3].add(new JLabel(new ImageIcon("./assets/queenblack.png")));
+    squares[0][4].add(new JLabel(new ImageIcon("./assets/kingblack.png")));
+    squares[0][5].add(new JLabel(new ImageIcon("./assets/bishopblack.png")));
+    squares[0][6].add(new JLabel(new ImageIcon("./assets/knightblack.png")));
+    squares[0][7].add(new JLabel(new ImageIcon("./assets/rookblack.png")));
 
-    squares[7][0].add(new JLabel(new ImageIcon("rookblue.png")));
-    squares[7][1].add(new JLabel(new ImageIcon("knightblue.png")));
-    squares[7][2].add(new JLabel(new ImageIcon("bishopblue.png")));
-    squares[7][3].add(new JLabel(new ImageIcon("queenblue.png")));
-    squares[7][4].add(new JLabel(new ImageIcon("kingblue.png")));
-    squares[7][5].add(new JLabel(new ImageIcon("bishopblue.png")));
-    squares[7][6].add(new JLabel(new ImageIcon("knightblue.png")));
-    squares[7][7].add(new JLabel(new ImageIcon("rookblue.png")));
+    squares[7][0].add(new JLabel(new ImageIcon("./assets/rookblue.png")));
+    squares[7][1].add(new JLabel(new ImageIcon("./assets/knightblue.png")));
+    squares[7][2].add(new JLabel(new ImageIcon("./assets/bishopblue.png")));
+    squares[7][3].add(new JLabel(new ImageIcon("./assets/queenblue.png")));
+    squares[7][4].add(new JLabel(new ImageIcon("./assets/kingblue.png")));
+    squares[7][5].add(new JLabel(new ImageIcon("./assets/bishopblue.png")));
+    squares[7][6].add(new JLabel(new ImageIcon("./assets/knightblue.png")));
+    squares[7][7].add(new JLabel(new ImageIcon("./assets/rookblue.png")));
 
     for (int i = 0; i < 8; i++) {
-        squares[1][i].add(new JLabel(new ImageIcon("pawnblack.png")));
-        squares[6][i].add(new JLabel(new ImageIcon("pawnblue.png")));
+        squares[1][i].add(new JLabel(new ImageIcon("./assets/pawnblack.png")));
+        squares[6][i].add(new JLabel(new ImageIcon("./assets/pawnblue.png")));
     }
 
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
