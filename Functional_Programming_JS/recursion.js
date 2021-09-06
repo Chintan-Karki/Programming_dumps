316813318020b5a0432a9454773ed0bdfa7bb88a
Chintan Karki <karkichintan2000@gmail.com>
2021-09-06 22:10:47 +0545

    functions performance comparison

--- Functional_Programming_JS/recursion.js
+++ Functional_Programming_JS/recursion.js
@@ -19,3 +19,65 @@ function sum_recursive(numbers){
 
 console.log(sum_iterative([1,2,3,4,5,6]))
 console.log(sum_recursive([1,2,3,4,5,6]))
+
+
+// FACTORIAL
+
+// ITERATIVE
+function factorialIterative(number){
+    let totalProduct = 1;
+    while (number > 0){
+        totalProduct = number * totalProduct;
+        number --;
+    }
+    return totalProduct;
+}
+
+// Recursive
+function factorialRecursive(number){
+    // Base Case:
+    if (number == 0) return 1;
+    // Recursive case
+    return number * factorialRecursive(number - 1);
+}
+console.time('factorialRecursive');
+console.log("Recursive Factorial: ",factorialRecursive(10))
+console.timeEnd('factorialRecursive');
+
+console.time('factorialIterative');
+console.log("Iterative Factorial: ",factorialIterative(10))
+console.timeEnd('factorialIterative');
+
+// Fibonacci 
+
+// ITERATIVE
+function iterativeFibo(n){
+    let a =1;
+    let b  = 1;
+    let c = 0;
+    if (n<=2) {
+        c = 1;
+    }else{
+        for (let i=1; i < n-1; i++){
+            c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+    return c;
+}
+
+function recursiveFibo(n){
+    // BASE CASE
+    if ( n <= 2 ) return 1;
+    // RECURSIVE CASE
+    return recursiveFibo(n-1) + recursiveFibo(n-2);
+}
+
+console.time('iterativeFibo');
+console.log("Iterative Fibonacci: ",iterativeFibo(30))
+console.timeEnd('iterativeFibo');
+
+console.time('recursiveFibo');
+console.log("Recursive Fibonacci: ",recursiveFibo(30)) //! Takes a lot of time
+console.timeEnd('recursiveFibo');
