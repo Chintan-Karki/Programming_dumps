107ed1101b7d8accf52512c524b4ae26c16777ba
Chintan Karki <karkichintan2000@gmail.com>
2021-09-27 21:34:01 +0545

    cheerio functions

--- scraping/app.js
+++ scraping/app.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const pretty = require("pretty");
 
+// Creating a basic markup
 const testMarkup = `
     <ul class = "fruits">
         <li class="fruit_mango">Mango</li>
@@ -9,22 +10,26 @@ const testMarkup = `
     </ul>
 `
 
+// Loading the markup in cheerio
 const $ = cheerio.load(testMarkup);
-// console.log(pretty($.html()));
+//* console.log(pretty($.html()));
 
 const mango = $(".fruit_mango");
-console.log(mango.html()); // Mango
+console.log(mango.html());  // Mango
 
 const apple = $(".fruit_apple");
 console.log(apple.attr("class"));
 
 const listItems = $("li");
 console.log(listItems.length);
+
+// Getting each list item from markup using .each() function
 listItems.each((index, el) => {
     console.log($(el).text());
     // console.log($(index))
 })
 
+// Appending and prepending ==> Cheerio
 const ul = $("ul");
 ul.append("<li>Banana</li>");
 ul.prepend("<li>Pineapple</li>");
